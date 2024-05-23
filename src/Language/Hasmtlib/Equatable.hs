dcd7a32e4f413dce36d5ab3867dc941163afea69
Julian.Bruder <julian.bruder@city-concept-leipzig.de>
2024-05-23 12:12:20 +0200

    master: added infix

--- src/Language/Hasmtlib/Equatable.hs
+++ src/Language/Hasmtlib/Equatable.hs
@@ -8,6 +8,7 @@ class Equatable a where
   (===) :: a -> a -> Expr BoolType
   (/==) :: a -> a -> Expr BoolType
   x /== y = not $ x === y
+infix 4 ===, /==
   
 instance Equatable (Expr a) where
   (===) = EQU
--- src/Language/Hasmtlib/Orderable.hs
+++ src/Language/Hasmtlib/Orderable.hs
@@ -9,6 +9,7 @@ class Equatable a => Orderable a where
   (<=?) :: a -> a -> Expr BoolType
   (>=?) :: a -> a -> Expr BoolType
   (>?)  :: a -> a -> Expr BoolType
+infix 4 <?, <=?, >=?, >?
 
 instance Orderable (Expr a) where
   (<?)  = LTH
