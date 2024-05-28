b4b60c3d1ef5bda0a9e0ddc17a34ce85e84d5f7d
Julian.Bruder <julian.bruder@city-concept-leipzig.de>
2024-05-28 11:16:21 +0200

    master: added Example.Basic3

---
+++ src/Language/Hasmtlib/Example/Basic3.hs
@@ -0,0 +1,21 @@
+module Language.Hasmtlib.Example.Basic3 where
+
+import Language.Hasmtlib
+import Control.Monad
+
+main :: IO ()
+main = do
+  res <- solveWith cvc5 $ do
+    setLogic "QF_LIA"
+
+    xs <- replicateM 100 $ var @IntType
+
+    forM_ (zip xs [1 :: Integer .. ]) $ \(x, i) -> assert $ x === fromInteger i
+  
+    return xs
+
+  case res of
+    (Sat, Just xs) -> print xs
+    _              -> putStrLn $ show res ++ " No solution"
+
+  return ()
