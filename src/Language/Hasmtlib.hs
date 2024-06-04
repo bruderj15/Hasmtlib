6afa2a78fb79853f0d905fe3a50d9ca6c404ec90
Julian.Bruder <julian.bruder@city-concept-leipzig.de>
2024-06-04 14:41:53 +0200

    master: added Variable class

--- src/Language/Hasmtlib.hs
+++ src/Language/Hasmtlib.hs
@@ -9,6 +9,7 @@ module Language.Hasmtlib
   , module Language.Hasmtlib.Equatable
   , module Language.Hasmtlib.Orderable
   , module Language.Hasmtlib.Codec
+  , module Language.Hasmtlib.Variable
   , module Language.Hasmtlib.Solver.CVC5
   , module Language.Hasmtlib.Solver.Z3
   , module Language.Hasmtlib.Solver.Yices
@@ -25,6 +26,7 @@ import Language.Hasmtlib.Boolean
 import Language.Hasmtlib.Equatable
 import Language.Hasmtlib.Orderable
 import Language.Hasmtlib.Codec
+import Language.Hasmtlib.Variable
 import Language.Hasmtlib.Solver.CVC5
 import Language.Hasmtlib.Solver.Z3
 import Language.Hasmtlib.Solver.Yices
---
+++ src/Language/Hasmtlib/Variable.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE DefaultSignatures #-}
+
+module Language.Hasmtlib.Variable where
+
+import Language.Hasmtlib.Internal.Expr
+import Language.Hasmtlib.Type.SMT
+import Control.Monad.State
+
+-- | Construct a datum of a data-type by creating variables for all its fields.
+--   Usage:
+--
+--    data V3 a = a a a
+--    instance Variable a => V3 a 
+--
+--    >>> varV3 <- variable @(V3 (Expr RealType)) ; varV3
+--        V3 (Var RealType) (Var RealType) (Var RealType)
+class Variable a where
+  variable :: MonadState SMT m => m a
+  default variable :: (MonadState SMT m, Applicative f, Traversable f, Variable b, a ~ f b) => m a
+  variable = sequence $ pure variable
+  
+instance KnownSMTRepr t => Variable (Expr t) where
+  variable = var
+
+instance Variable () where
+  variable = return ()
+
+instance (Variable a, Variable b) => Variable (a,b) where
+  variable = (,) <$> variable <*> variable
+
+instance (Variable a, Variable b, Variable c) => Variable (a,b,c) where
+  variable = (,,) <$> variable <*> variable  <*> variable
+
+instance (Variable a, Variable b, Variable c, Variable d) => Variable (a,b,c,d) where
+  variable = (,,,) <$> variable <*> variable <*> variable <*> variable
+
+instance (Variable a, Variable b, Variable c, Variable d, Variable e) => Variable (a,b,c,d,e) where
+  variable = (,,,,) <$> variable <*> variable <*> variable <*> variable <*> variable
+
+instance (Variable a, Variable b, Variable c, Variable d, Variable e, Variable f) => Variable (a,b,c,d,e,f) where
+  variable = (,,,,,) <$> variable <*> variable <*> variable <*> variable <*> variable <*> variable
+
+instance (Variable a, Variable b, Variable c, Variable d, Variable e, Variable f, Variable g) => Variable (a,b,c,d,e,f,g) where
+  variable = (,,,,,,) <$> variable <*> variable <*> variable <*> variable <*> variable <*> variable <*> variable
+
+instance (Variable a, Variable b, Variable c, Variable d, Variable e, Variable f, Variable g, Variable h) => Variable (a,b,c,d,e,f,g,h) where
+  variable = (,,,,,,,) <$> variable <*> variable <*> variable <*> variable <*> variable <*> variable <*> variable <*> variable
+
+instance Variable a => Variable (Maybe a)
+instance Variable b => Variable (Either a b)
