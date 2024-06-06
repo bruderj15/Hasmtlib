c0fd2710d18bbc9bc5fcc2829065a0483b2f470a
Julian.Bruder <julian.bruder@city-concept-leipzig.de>
2024-06-06 12:54:52 +0200

    unsigned-bv: added bitvec doc

--- src/Language/Hasmtlib/Internal/Bitvec.hs
+++ src/Language/Hasmtlib/Internal/Bitvec.hs
@@ -14,6 +14,7 @@ import Data.Bifunctor
 import qualified Data.Vector.Unboxed.Sized as V
 import GHC.TypeNats
 
+-- | MSB first length-indexed Bitvector
 newtype Bitvec (n :: Nat) = Bitvec { unBitvec :: V.Vector n Bit }
   deriving stock (Eq, Ord)
   deriving newtype (Boolean)
