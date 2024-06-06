39db990c1f96a4172a28e8d4fe92649051dbd053
Julian.Bruder <julian.bruder@city-concept-leipzig.de>
2024-06-06 12:52:56 +0200

    unsigned-bv: added extra bv operations

--- src/Language/Hasmtlib/Type/Expr.hs
+++ src/Language/Hasmtlib/Type/Expr.hs
@@ -5,9 +5,33 @@ module Language.Hasmtlib.Type.Expr
  , Value(..), extractValue, putValue
  , Repr(..), KnownSMTRepr(..), SomeKnownSMTRepr(..)
  , Expr
- , module Language.Hasmtlib.Internal.Expr.Num 
+ , module Language.Hasmtlib.Internal.Expr.Num
+ , bvuDiv, bvuRem, bvShL, bvLShR, bvConcat, bvRotL, bvRotR 
  )
 where
 
 import Language.Hasmtlib.Internal.Expr
 import Language.Hasmtlib.Internal.Expr.Num
+import Data.Proxy
+import GHC.TypeNats
+
+bvuDiv   :: KnownNat n => Expr (BvType n) -> Expr (BvType n) -> Expr (BvType n)
+bvuDiv   = BvuDiv
+
+bvuRem   :: KnownNat n => Expr (BvType n) -> Expr (BvType n) -> Expr (BvType n)
+bvuRem   = BvuRem
+
+bvShL    :: KnownNat n => Expr (BvType n) -> Expr (BvType n) -> Expr (BvType n)
+bvShL    = BvShL
+
+bvLShR   :: KnownNat n => Expr (BvType n) -> Expr (BvType n) -> Expr (BvType n)
+bvLShR   = BvLShR
+
+bvConcat :: (KnownNat n, KnownNat m) => Expr (BvType n) -> Expr (BvType m) -> Expr (BvType (n + m))
+bvConcat = BvConcat
+
+bvRotL   :: (KnownNat n, KnownNat i, KnownNat (Mod i n)) => Proxy i -> Expr (BvType n) -> Expr (BvType n)
+bvRotL   = BvRotL
+
+bvRotR   :: (KnownNat n, KnownNat i, KnownNat (Mod i n)) => Proxy i -> Expr (BvType n) -> Expr (BvType n)
+bvRotR   = BvRotR
