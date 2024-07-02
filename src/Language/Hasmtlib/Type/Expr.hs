bc687c8edbe6616c8c720ccaa5f5f045d7b3ae26
Julian.Bruder <julian.bruder@city-concept-leipzig.de>
2024-07-02 16:53:11 +0200

    clean: export AllC

--- src/Language/Hasmtlib/Type/Expr.hs
+++ src/Language/Hasmtlib/Type/Expr.hs
@@ -5,7 +5,7 @@ module Language.Hasmtlib.Type.Expr
  , SMTVar(..), varId
  , HaskellType
  , Value(..), unwrapValue, wrapValue
- , SSMTSort(..), KnownSMTSort(..), sortSing', SomeSMTSort(..), SomeKnownSMTSort
+ , SSMTSort(..), KnownSMTSort(..), sortSing', SomeSMTSort(..), SomeKnownSMTSort, AllC
  , Expr
  , for_all , exists
  , select, store
