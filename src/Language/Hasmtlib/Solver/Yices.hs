a91e4fc035cd1cc2f2b62b0227ff734cc4cf75db
bruderj15 <julian.bruder@outlook.com>
2024-07-26 11:18:46 +0200

    clean: added incremental-flag to yices

--- src/Language/Hasmtlib/Solver/Yices.hs
+++ src/Language/Hasmtlib/Solver/Yices.hs
@@ -6,4 +6,4 @@ import qualified SMTLIB.Backends.Process as P
 -- | A 'ProcessSolver' for Yices.
 --   Requires binary @yices-smt2@ to be in path.
 yices :: ProcessSolver
-yices = ProcessSolver $ P.defaultConfig { P.exe = "yices-smt2", P.args = ["--smt2-model-format"] }
+yices = ProcessSolver $ P.defaultConfig { P.exe = "yices-smt2", P.args = ["--smt2-model-format", "--incremental"] }
