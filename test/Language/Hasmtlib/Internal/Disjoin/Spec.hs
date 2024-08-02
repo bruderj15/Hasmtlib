49399d3157a394b9fb87344bf2aba8c9639a40a8
bruderj15 <julian.bruder@outlook.com>
2024-08-02 12:57:19 +0200

    #41-disjunct: added first actual test

--- test/Language/Hasmtlib/Internal/Disjoin/Spec.hs
+++ test/Language/Hasmtlib/Internal/Disjoin/Spec.hs
@@ -6,10 +6,12 @@ import Language.Hasmtlib.Internal.Expr.Arbitrary
 import Test.QuickCheck.Arbitrary
 import Test.Hspec.QuickCheck
 import Test.Hspec
-import Data.Sequence
+import Data.Sequence as Seq
+import Control.Monad
+import Control.Lens
 
 main :: IO ()
 main = hspec $ do
   describe "disjoin" $ do
       prop "does not alter the amount of asserted formulas" $
-        \(x :: Seq (Expr BoolSort)) -> True
+        \(s :: SMT) -> Seq.length (s^.formulas) == sum (Seq.length . view formulas <$> disjoin s)
