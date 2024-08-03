a2a6abcdce560bcf069d073069f7bd8e0183bb68
bruderj15 <julian.bruder@outlook.com>
2024-08-03 12:24:50 +0200

    #41-disjunct: When testing generate vars before
    arbitrary SMTVar on each occasion leads to SMTVars with different phantom types (t :: SMTSort) but the same varId. To avoid, bound varIds to fixed SMTSorts before

--- test/Language/Hasmtlib/Arbitrary.hs
+++ test/Language/Hasmtlib/Arbitrary.hs
@@ -11,6 +11,7 @@ import Test.QuickCheck hiding ((===), (==>))
 import Data.Proxy
 import Data.Coerce
 import Data.Maybe
+import qualified Data.Dependent.Map as DMap
 import qualified Data.IntSet as IntSet
 import qualified Data.Sequence as Seq
 import Control.Monad
@@ -23,86 +24,93 @@ instance (Ord k, Arbitrary k, Arbitrary v) => Arbitrary (ConstArray k v) where
   arbitrary = liftM2 ConstArray arbitrary arbitrary
 
 instance (KnownSMTSort t, Arbitrary (HaskellType t)) => Arbitrary (Expr t) where
-  arbitrary = sized sizedArbitraryExpr
+  arbitrary =
+    let varMap =  DMap.singleton SBoolSort (SMTVarList [SMTVar i | i <- [1..10]]) `DMap.union`
+                  DMap.singleton SIntSort  (SMTVarList [SMTVar i | i <- [11..20]]) `DMap.union`
+                  DMap.singleton SRealSort  (SMTVarList [SMTVar i | i <- [21..30]]) `DMap.union`
+                  DMap.singleton (SBvSort (Proxy @8))  (SMTVarList [SMTVar i | i <- [31..40]])
+     in sized (sizedArbitraryExpr varMap)
 
-sizedArbitraryExpr :: forall t. (KnownSMTSort t, Arbitrary (HaskellType t)) => Int -> Gen (Expr t)
-sizedArbitraryExpr 0 = oneof
+newtype SMTVarList (t :: SMTSort) = SMTVarList { unList :: [SMTVar t] }
+
+sizedArbitraryExpr :: forall t. (KnownSMTSort t, Arbitrary (HaskellType t)) => DMap.DMap SSMTSort SMTVarList -> Int -> Gen (Expr t)
+sizedArbitraryExpr varMap 0 = oneof
   [
     constant <$> arbitrary
-  , Var . SMTVar . getPositive <$> arbitrary
+  , maybe (constant <$> arbitrary) (oneof . fmap (return . Var) . unList) $ DMap.lookup (sortSing @t) varMap
   ]
-sizedArbitraryExpr n = case sortSing @t of
+sizedArbitraryExpr varMap n = case sortSing @t of
   SBoolSort      -> oneof
     [
-      liftM2 (<?)  (sizedArbitraryExpr @IntSort     $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (<?)  (sizedArbitraryExpr @RealSort    $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (<?)  (sizedArbitraryExpr @(BvSort 8)  $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (<=?) (sizedArbitraryExpr @IntSort    $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (<=?) (sizedArbitraryExpr @RealSort   $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (<=?) (sizedArbitraryExpr @(BvSort 8) $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (===) (sizedArbitraryExpr @IntSort    $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (===) (sizedArbitraryExpr @RealSort   $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (===) (sizedArbitraryExpr @(BvSort 8) $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (/==) (sizedArbitraryExpr @IntSort    $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (/==) (sizedArbitraryExpr @RealSort   $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (/==) (sizedArbitraryExpr @(BvSort 8) $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (>=?) (sizedArbitraryExpr @IntSort    $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (>=?) (sizedArbitraryExpr @RealSort   $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (>=?) (sizedArbitraryExpr @(BvSort 8) $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (>?)  (sizedArbitraryExpr @IntSort     $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (>?)  (sizedArbitraryExpr @RealSort    $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (>?)  (sizedArbitraryExpr @(BvSort 8)  $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , fmap   not   (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (&&)  (sizedArbitraryExpr $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (||)  (sizedArbitraryExpr $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 (==>) (sizedArbitraryExpr $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 xor   (sizedArbitraryExpr $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , fmap isIntSort (sizedArbitraryExpr $ n `div` 2)
-    , liftM3 ite     (sizedArbitraryExpr @BoolSort $ n `div` 2) (sizedArbitraryExpr $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 select  (sizedArbitraryExpr @(ArraySort BoolSort _)   $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 select  (sizedArbitraryExpr @(ArraySort IntSort _)    $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 select  (sizedArbitraryExpr @(ArraySort RealSort _)   $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 select  (sizedArbitraryExpr @(ArraySort (BvSort 8) _) $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
+      liftM2 (<?)  (sizedArbitraryExpr @IntSort varMap   $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (<?)  (sizedArbitraryExpr @RealSort varMap   $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (<?)  (sizedArbitraryExpr @(BvSort 8) varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (<=?) (sizedArbitraryExpr @IntSort varMap   $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (<=?) (sizedArbitraryExpr @RealSort varMap   $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (<=?) (sizedArbitraryExpr @(BvSort 8) varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (===) (sizedArbitraryExpr @IntSort varMap   $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (===) (sizedArbitraryExpr @RealSort varMap   $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (===) (sizedArbitraryExpr @(BvSort 8) varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (/==) (sizedArbitraryExpr @IntSort varMap   $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (/==) (sizedArbitraryExpr @RealSort varMap   $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (/==) (sizedArbitraryExpr @(BvSort 8) varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (>=?) (sizedArbitraryExpr @IntSort varMap   $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (>=?) (sizedArbitraryExpr @RealSort varMap   $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (>=?) (sizedArbitraryExpr @(BvSort 8) varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (>?)  (sizedArbitraryExpr @IntSort varMap   $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (>?)  (sizedArbitraryExpr @RealSort varMap   $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (>?)  (sizedArbitraryExpr @(BvSort 8) varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , fmap   not   (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (&&)  (sizedArbitraryExpr varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (||)  (sizedArbitraryExpr varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 (==>) (sizedArbitraryExpr varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 xor   (sizedArbitraryExpr varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , fmap isIntSort (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM3 ite     (sizedArbitraryExpr @BoolSort varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 select  (sizedArbitraryExpr @(ArraySort BoolSort _) varMap   $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 select  (sizedArbitraryExpr @(ArraySort IntSort _) varMap    $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 select  (sizedArbitraryExpr @(ArraySort RealSort _) varMap   $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 select  (sizedArbitraryExpr @(ArraySort (BvSort 8) _) varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
     ]
   SIntSort       -> oneof $
-    numExprGens n ++ [
-      fmap toIntSort (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 mod (sizedArbitraryExpr $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 div (sizedArbitraryExpr $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
+    numExprGens varMap n ++ [
+      fmap toIntSort (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 mod (sizedArbitraryExpr varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 div (sizedArbitraryExpr varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
     ]
   SRealSort      -> oneof $
-    numExprGens n ++ [
-      liftM2 (/) (sizedArbitraryExpr $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , fmap toRealSort (sizedArbitraryExpr $ n `div` 2)
-    , fmap sqrt (sizedArbitraryExpr $ n `div` 2)
-    , fmap exp  (sizedArbitraryExpr $ n `div` 2)
-    , fmap sin  (sizedArbitraryExpr $ n `div` 2)
-    , fmap cos  (sizedArbitraryExpr $ n `div` 2)
-    , fmap tan  (sizedArbitraryExpr $ n `div` 2)
-    , fmap asin (sizedArbitraryExpr $ n `div` 2)
-    , fmap acos (sizedArbitraryExpr $ n `div` 2)
-    , fmap atan (sizedArbitraryExpr $ n `div` 2)
+    numExprGens varMap n ++ [
+      liftM2 (/) (sizedArbitraryExpr varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , fmap toRealSort (sizedArbitraryExpr varMap $ n `div` 2)
+    , fmap sqrt (sizedArbitraryExpr varMap $ n `div` 2)
+    , fmap exp  (sizedArbitraryExpr varMap $ n `div` 2)
+    , fmap sin  (sizedArbitraryExpr varMap $ n `div` 2)
+    , fmap cos  (sizedArbitraryExpr varMap $ n `div` 2)
+    , fmap tan  (sizedArbitraryExpr varMap $ n `div` 2)
+    , fmap asin (sizedArbitraryExpr varMap $ n `div` 2)
+    , fmap acos (sizedArbitraryExpr varMap $ n `div` 2)
+    , fmap atan (sizedArbitraryExpr varMap $ n `div` 2)
     , return $ constant pi
     ]
   SBvSort _      -> oneof $
-    numExprGens n ++ [
-      liftM2 mod (sizedArbitraryExpr $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-    , liftM2 div (sizedArbitraryExpr $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
+    numExprGens varMap n ++ [
+      liftM2 mod (sizedArbitraryExpr varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+    , liftM2 div (sizedArbitraryExpr varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
     ]
-  SArraySort _ _ -> sizedArbitraryExpr 0
+  SArraySort _ _ -> sizedArbitraryExpr varMap 0
 
-numExprGens :: (Num (Expr t), KnownSMTSort t, Arbitrary (HaskellType t)) => Int -> [Gen (Expr t)]
-numExprGens n =
+numExprGens :: (Num (Expr t), KnownSMTSort t, Arbitrary (HaskellType t)) => DMap.DMap SSMTSort SMTVarList -> Int -> [Gen (Expr t)]
+numExprGens varMap n =
   [
-    fmap negate (sizedArbitraryExpr $ n `div` 2)
-  , liftM2 (+)  (sizedArbitraryExpr $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-  , liftM2 (-)  (sizedArbitraryExpr $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-  , liftM2 (*)  (sizedArbitraryExpr $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-  , liftM3 ite  (sizedArbitraryExpr @BoolSort $ n `div` 2) (sizedArbitraryExpr $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-  , liftM2 select (sizedArbitraryExpr @(ArraySort BoolSort _)   $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-  , liftM2 select (sizedArbitraryExpr @(ArraySort IntSort _)    $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-  , liftM2 select (sizedArbitraryExpr @(ArraySort RealSort _)   $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
-  , liftM2 select (sizedArbitraryExpr @(ArraySort (BvSort 8) _) $ n `div` 2) (sizedArbitraryExpr $ n `div` 2)
+    fmap negate (sizedArbitraryExpr varMap $ n `div` 2)
+  , liftM2 (+)  (sizedArbitraryExpr varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+  , liftM2 (-)  (sizedArbitraryExpr varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+  , liftM2 (*)  (sizedArbitraryExpr varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+  , liftM3 ite  (sizedArbitraryExpr @BoolSort varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+  , liftM2 select (sizedArbitraryExpr @(ArraySort BoolSort _) varMap   $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+  , liftM2 select (sizedArbitraryExpr @(ArraySort IntSort _) varMap    $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+  , liftM2 select (sizedArbitraryExpr @(ArraySort RealSort _) varMap   $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
+  , liftM2 select (sizedArbitraryExpr @(ArraySort (BvSort 8) _) varMap $ n `div` 2) (sizedArbitraryExpr varMap $ n `div` 2)
   ]
 
 instance Arbitrary SMT where
