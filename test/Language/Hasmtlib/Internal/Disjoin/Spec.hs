566ad1f727d6103900bfb90090c0617a97da786a
bruderj15 <julian.bruder@outlook.com>
2024-08-03 11:20:16 +0200

    #41-disjunct: added another prop for Disjoint

--- test/Language/Hasmtlib/Internal/Disjoin/Spec.hs
+++ test/Language/Hasmtlib/Internal/Disjoin/Spec.hs
@@ -1,12 +1,14 @@
 module Language.Hasmtlib.Internal.Disjoin.Spec where
 
-import Language.Hasmtlib
+import Language.Hasmtlib hiding ((&&))
 import Language.Hasmtlib.Internal.Disjoin
 import Language.Hasmtlib.Arbitrary
 import Test.QuickCheck.Arbitrary
 import Test.Hspec.QuickCheck
 import Test.Hspec
 import Data.Sequence as Seq
+import Data.IntSet as IntSet
+import Data.Foldable as Foldable
 import Control.Monad
 import Control.Lens
 
@@ -14,6 +16,14 @@ main :: IO ()
 main = hspec $ do
   describe "disjoin" $ do
       prop "does not alter the amount of asserted formulas" $
-        \(s :: SMT) -> Seq.length (s^.formulas) == sum (Seq.length . view formulas <$> disjoin s)
+        \(s :: SMT) -> Seq.length (s^.formulas) == Seq.length (disjoin s ^.folded.formulas)
       prop "does not alter the amount of variables" $
-        \(s :: SMT) -> Seq.length (s^.vars) == sum (Seq.length . view vars <$> disjoin s)
+        \(s :: SMT) -> Seq.length (s^.vars) == Seq.length (disjoin s ^.folded.vars)
+      prop "transforms a single SMT-state into many disjoint ones" $
+        \(s :: SMT) -> fst $
+            Foldable.foldr
+              (\vs' (b, vs) -> (b && IntSet.disjoint vs vs', vs <> vs'))
+              (True, mempty) $
+              fmap
+                (IntSet.fromList . Foldable.toList . fmap (\(SomeSMTSort (SMTVar i)) -> i) . view vars) $
+                disjoin s
