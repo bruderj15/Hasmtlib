19343456f32738a7079769548bbe33e504abfb72
bruderj15 <julian.bruder@outlook.com>
2024-08-16 18:28:07 +0200

    43-observable-sharing: added Uniplate1#lazyParaM1

--- src/Language/Hasmtlib/Internal/Uniplate1.hs
+++ src/Language/Hasmtlib/Internal/Uniplate1.hs
@@ -11,3 +11,6 @@ class Uniplate1 f cs | f -> cs where
 
 transformM1 :: (Monad m, Uniplate1 f cs, AllC cs b) => (forall a. AllC cs a => f a -> m (f a)) -> f b -> m (f b)
 transformM1 f x = uniplate1 (transformM1 f) x >>= f
+
+lazyParaM1 :: (Monad m, Uniplate1 f cs, AllC cs b) => (forall a. AllC cs a => f a -> m (f a) -> m (f a)) -> f b -> m (f b)
+lazyParaM1 f x = f x (uniplate1 (lazyParaM1 f) x)
